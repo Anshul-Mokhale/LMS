de8fc69e62eb81926850235013684c6b87107f91
Anshul Mokhale <89470956+Anshul-Mokhale@users.noreply.github.com>
2024-06-21 17:03:03 +0530

    update 2

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -16,8 +16,55 @@
         </div>
     </div>
     <div class="lms-home-2">
-
+        <h1>Features</h1>
+        <div class="feature-box">
+            <div class="feature-box-1">
+                <div class="feature-box-1-left">
+                    <div class="feature-box-1-left-1">
+                        <img src={{ asset('img/meeting.svg') }} alt="meet">
+                        <div>
+                            <h3>Live Sessions</h3>
+                            <p>Schedule and conduct live interactive sessions.</p>
+                        </div>
+                    </div>
+                    <div class="feature-box-1-left-2">
+                        <img src={{ asset('img/test.svg') }} alt="test">
+                        <div>
+                            <h3>Assignments & Tests</h3>
+                            <p>Manage assignments and tests efficiently.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="feature-box-1-right">
+                    <img src={{ asset('img/codeing.svg') }} alt="code">
+                    <div>
+                        <h3>Online Coding Platform</h3>
+                        <p>Integrated platform for coding practice and assignments.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
+    <div class="lms-home-2">
+        <h1>How it works</h1>
+        <div class="method-box">
+            <div class="method-box-1">
+                <img src={{ asset('img/Signup.svg') }} alt="Signup">
+                <h3>Institute Registration</h3>
+            </div>
+            <div class="method-box-2">
+                <img src={{ asset('img/plan.svg') }} alt="Pricing">
+                <h3>Select Plan</h3>
+            </div>
+            <div class="method-box-2">
+                <img src={{ asset('img/create_grp.svg') }} alt="class">
+                <h3>Create Course</h3>
+            </div>
+        </div>
+    </div>
+
 </div>
+
+
 
 @include('footer')
